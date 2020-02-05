3c3a46a73c290ddbae2d20f8c86e59b533f5ad8b
Dalep <dalepaulsen.code.social@gmail.com>
2020-02-05 12:43:36 +1000

    connected facebook footer icon to client facebook page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,15 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <a href="/login"><p className="admin-login">Admin Login</p></a>
-        <p>©2020 <b>Pesky Pooch</b> Training Ltd.</p>
-        <p className="social-icons"><i className="fab fa-facebook" /></p>
+        <a href="/login">
+          <p className="admin-login">Admin Login</p>
+        </a>
+        <p>
+          ©2020 <b>Pesky Pooch</b> Training Ltd.
+        </p>
+        <p className="social-icons">
+          <a href="https://www.facebook.com/PeskyPooch/" alt="pesky pooch facebook" target="_blank"><i className="fab fa-facebook"></i></a>
+          </p>
       </footer>
     );
   }
